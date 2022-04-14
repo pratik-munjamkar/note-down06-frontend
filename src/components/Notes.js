c5adc39d5c91ccf6a5baae1689f7a60d05f23928
pratik0626 <89349824+pratik-munjamkar@users.noreply.github.com>
2022-04-14 18:22:28 +0530

    Add files via upload

---
+++ src/components/Notes.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Header from './notes/Nav'
+import Home from './notes/Home'
+import CreateNote from './notes/CreateNote'
+import EditNote from './notes/EditNote'
+import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
+
+export default function Notes({setIsLogin}) {
+    return (
+        <Router>
+        <div className="notes-page">
+            <Header setIsLogin={setIsLogin} />
+            <section>
+                <Routes>
+                <Route path="/" element={<Home/>} exact />
+                <Route path="/create" element={<CreateNote/>} exact />
+                <Route path="/edit/:id" element={<EditNote/>} exact />
+                </Routes>
+            </section>
+            
+        </div>
+        </Router>
+    )
+}
